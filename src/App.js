711ffe74e19e9133ba6c222831251c93d7af237f
begazj <102997473+begazj@users.noreply.github.com>
2022-08-16 09:46:05 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather Readings
-      <footer>
-        This project was coded by Jen Begazo and is{" "}
-        <a
-          href="https://github.com/begazj/weather-react-jb"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-        on Github
-      </footer>
+      <div className="container">
+        Weather Readings
+        <footer>
+          This project was coded by Jen Begazo and is{" "}
+          <a
+            href="https://github.com/begazj/weather-react-jb"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on Github
+        </footer>
+      </div>
     </div>
   );
 }
