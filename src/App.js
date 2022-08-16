821128759d7aedca049a5e3848734b6038a0542a
begazj <102997473+begazj@users.noreply.github.com>
2022-08-15 20:36:54 -0400

    Added Github repo Url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather </div>;
+  return (
+    <div className="App">
+      Weather Readings
+      <footer>
+        This project was coded by Jen Begazo and is{" "}
+        <a
+          href="https://github.com/begazj/weather-react-jb"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on Github
+      </footer>
+    </div>
+  );
 }
