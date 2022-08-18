95e224fea097dfaf4713fa30f236cd101fa95531
begazj <102997473+begazj@users.noreply.github.com>
2022-08-18 15:38:18 -0400

    Added api integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <Weather defaultCity="Berlin" />
 
       <footer>
         This project was coded by Jen Begazo and is{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,76 +1,93 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import axios from "axios";
 
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather shadow">
-      <div className="container">
-        <div className="box with-linear-gradient">
-          <h1>Weather Readings</h1>
-          <div className="crystal-ball shadow">
-            <div className="fade-in-text">
-              <div className="row current-temp">
-                <div className="col">
-                  <div className="clearfix">
-                    <img
-                      src=" https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                      alt="Sunny"
-                    ></img>
-                    <span className="temperature">6</span>
-                    <span className="unit">°C</span>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      date: "Wednesday 07:00",
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="Weather shadow-lg">
+        <div className="container">
+          <div className="box with-linear-gradient">
+            <h1>Weather Readings</h1>
+            <div className="crystal-ball shadow">
+              <div className="fade-in-text">
+                <div className="row current-temp">
+                  <div className="col">
+                    <div className="clearfix">
+                      <img
+                        src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                        alt="Sunny"
+                      ></img>
+                      <span className="temperature">
+                        {Math.round(weatherData.temperature)}
+                      </span>
+                      <span className="unit">°C</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <ul className="weather-description">
-                <li>Wedensday 12:13</li>
-                <li>Sunny</li>
-                <li>Preciptation: 15%</li>
-                <li>Humidity: 15%</li>
-                <li>Wind: 13 km/h</li>
-              </ul>
-            </div>
-          </div>
-          <form className="d-flex">
-            <div className="row m-1">
-              <div className="col-6">
-                <input
-                  type="search"
-                  placeHolder="Enter a city..."
-                  className="form-control"
-                  autoFocus="on"
-                />
-              </div>
-              <div className="col-3">
-                <input
-                  type="submit"
-                  value="Search"
-                  className="submit-button btn btn-primary w-100"
-                />
-              </div>
-              <div className="col-3">
-                <input
-                  type="submit"
-                  value="Current"
-                  className="current-button btn btn-primary w-100"
-                />
+                <h2>{weatherData.city}</h2>
+                <ul className="weather-description">
+                  <li>{weatherData.date}</li>
+                  <li className="text-capitalize">{weatherData.description}</li>
+                  <li>Humidity: {weatherData.humidity}%</li>
+                  <li>Wind: {weatherData.wind} km/h</li>
+                </ul>
               </div>
             </div>
-          </form>
-          <hr />
-          <div className="row">
-            <div className="col">
-              <h2>New York</h2>
-              <h4>Daily Guidance</h4>
-            </div>
-            <div>
-              <FontAwesomeIcon icon="fa-star-and-crescent" />
-              <FontAwesomeIcon icon="fa-solid fa-star" />
+            <form className="d-flex">
+              <div className="row m-1">
+                <div className="col-6">
+                  <input
+                    type="search"
+                    placeHolder="Enter a city..."
+                    className="form-control shadow-sm"
+                    autoFocus="on"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="submit-button btn btn-primary w-100 shadow-sm"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Current"
+                    className="current-button btn btn-primary w-100 shadow-sm"
+                  />
+                </div>
+              </div>
+            </form>
+            <hr />
+            <div className="row">
+              <div className="col">
+                <h4>Daily Guidance</h4>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "7606076d1271fb4e58f13d2dd877419c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Calling the universe...";
+  }
 }
